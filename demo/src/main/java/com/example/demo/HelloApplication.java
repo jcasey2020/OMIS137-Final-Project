2ecf8cd43605bf27c8007f260c5f2533e7c06a25
jcasey2020 <115436326+jcasey2020@users.noreply.github.com>
2023-03-13 15:18:30 -0700

    Add flower image to garden grid

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    /*
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -17,7 +23,30 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+     */
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane gridPane = new GridPane();
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                ImageView imageView = new ImageView();
+                imageView.setFitHeight(50);
+                imageView.setFitWidth(50);
+                Image image = new Image(new FileInputStream("C:\\Users\\jfran\\IdeaProjects\\simple-flower-pink-hi.png"));
+                imageView.setImage(image);
+                gridPane.add(imageView, i, j);
+            }
+        }
+
+        Scene scene = new Scene(gridPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
+
